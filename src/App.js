5559fd59d08b7dd4c9ae2bdf43453361138fffaf
Serotoninene <pujol.alexandre@hotmail.fr>
2022-03-28 17:25:58 +0200

    cleaning the set up file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import { Suspense, useRef } from "react";
-// Three
-import { TextureLoader } from "three";
 // R3F + Drei
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-// Assets
-import cakes from "./Assets/Images/cakes.jpg";
-import canap from "./Assets/Images/canap.jpg";
-import eliott from "./Assets/Images/eliott.jpg";
 // Styling
 import "./Scss/style.scss";
 
 function Scene() {
-  const spriteRef = useRef();
-  const texture = useLoader(TextureLoader, cakes);
-  console.log(spriteRef.current);
-  useFrame((state, delta) => {});
+  const meshRef = useRef();
   return (
-    <sprite ref={spriteRef} scale={[5, 5]}>
-      <spriteMaterial map={texture} />
-    </sprite>
+    <mesh ref={meshRef}>
+      <octahedronBufferGeometry />
+      <meshNormalMaterial />
+    </mesh>
   );
 }
 
